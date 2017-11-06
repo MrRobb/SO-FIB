f40a2fc07b625c09d6fbb0b4bf1507239247fb34
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-11-06 16:34:28 +0100

    Práctica 1 y 2

--- S2/previo/listaParametros.c
+++ S2/previo/listaParametros.c
@@ -2,15 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void Usage() {
-	printf("Usage:listaParametros arg1 [arg2..argn]\n");
-	printf("Este programa escribe por su salida la lista de argumentos que recibe");	
-}
-
 int main(int argc,char *argv[])
 {
 	char buf[80];
-	if (argc <= 1) Usage();
 	for (int i=0;i<argc;i++){
 		if (i==0) {
 			sprintf(buf,"El argumento %d es %s (es el nombre del ejecutable)\n",i,argv[i]);
@@ -22,5 +16,3 @@ int main(int argc,char *argv[])
 	}
 	return 0;
 }
-
-
